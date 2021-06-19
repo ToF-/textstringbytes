6277c37372db89d621fb3b457acc6446e2d86f5c
Christophe Thibaut <cthibaut@octo.com>
2021-06-19 18:19:30 +0200

    converting String to Text to ByteString

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,45 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
-import Lib
+import GHC.Generics                     (Generic)
+import System.Environment               (getArgs)
+import System.Exit                      (die, exitSuccess)
+import Control.Monad                    (unless)
+import Data.Text
+import Data.Text.Encoding
+import Data.ByteString
+
+
+
+data Content = Content { content :: Text
+                       , size    :: Int  }
+    deriving (Show)
+
+
+
+stringToText :: String -> Text
+stringToText = Data.Text.pack
+
+textToByteString :: Text -> ByteString
+textToByteString = encodeUtf8
+
+byteStringLength :: ByteString -> Int
+byteStringLength = Data.ByteString.length
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    unless (Prelude.length args == 2)
+        (die $ "usage: textstringbytes-exe \"some text\" filename")
+
+    let arg_as_string = args !! 0
+        arg_as_text = stringToText arg_as_string
+        arg_as_bytestring = textToByteString arg_as_text
+        content   = Content arg_as_text (byteStringLength arg_as_bytestring)
+        file_path = args !! 1
+    Prelude.writeFile file_path (show content)
+
+
+
+
+
